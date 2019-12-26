407ca66250c9a7767cbd615136a48f0596888208
abhinav1311 <purohitabhinav2000@gmail.com>
2019-12-26 15:24:04 +0530

    3 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h1>Hello {{name}}!</h1>
+  <h2> age is {{age}} </h2>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
   @Input() name: string;
+  @Input() age: number;
 }
